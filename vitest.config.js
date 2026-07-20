86235257210dd59ff3c8cdadd9edd2b03e687e61
TheeZeeOhh <azizaokoro518@gmail.com>
2026-07-20 01:29:07 -0400

    Revert "Add Lumen: a fully test-verified toy language (demo/lumen)"
    This reverts commit a5988fcd1b8604988f662b5e0c11ef585ad769dc.

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     environment: 'node',
     setupFiles: ['./vitest.setup.js'],
-    include: ['src/**/*.test.js', 'demo/**/*.test.js']
+    include: ['src/**/*.test.js']
   }
 });
